c4c1cef0c9c6e189a7b012957107bcea928ae3ef
Richard Porter <rpwebdevelopment@gmail.com>
2024-09-24 17:12:49 +0100

    updated render to import new modules

--- src/renderer.js
+++ src/renderer.js
@@ -27,7 +27,14 @@
  */
 
 import { createApp } from "vue";
+import VueSlider from "vue-slider-component";
+import 'vue-slider-component/theme/default.css';
+import CodeEditor from "simple-code-editor";
+
 import App from "./components/App.vue";
 import './index.css';
 
-createApp(App).mount('#app');
+createApp(App)
+    .component('VueSlider', VueSlider)
+    .component('code-editor', CodeEditor)
+    .mount('#app');
